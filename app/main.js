8e140b67210db6de40277671a012a76d67a63b19
Marcelo Alves <selfmadecelo@gmail.com>
2015-10-11 12:56:42 -0700

    Fix indentation

--- app/main.js
+++ app/main.js
@@ -3,92 +3,92 @@ var TodoItems = require('./todoitems');
 
 var Todo = React.createClass({
 
-	getInitialState: function() {
-		return {
-			"todoItems": [
-				{"item": "Test Item 1", "completed": false, "isEditMode": false},
-				{"item": "Test Item 2", "completed": false, "isEditMode": false},
-				{"item": "Test Item 3", "completed": false, "isEditMode": false},
-				{"item": "Test Item 4", "completed": true, "isEditMode": false}
-			]
-		};
-	},
+  getInitialState: function() {
+    return {
+      "todoItems": [
+        {"item": "Test Item 1", "completed": false, "isEditMode": false},
+        {"item": "Test Item 2", "completed": false, "isEditMode": false},
+        {"item": "Test Item 3", "completed": false, "isEditMode": false},
+        {"item": "Test Item 4", "completed": true, "isEditMode": false}
+      ]
+    };
+  },
 
-	handleSubmit: function(e){
-		e.preventDefault();
+  handleSubmit: function(e){
+    e.preventDefault();
 
-		var item = {
-			"item": React.findDOMNode(this.refs.todoitem).value,
-			"completed": false
-		};
+    var item = {
+      "item": React.findDOMNode(this.refs.todoitem).value,
+      "completed": false
+    };
 
-		this.setState({
-			"todoItems": this.state.todoItems.concat(item) 
-		});
+    this.setState({
+      "todoItems": this.state.todoItems.concat(item) 
+    });
 
-		React.findDOMNode(this.refs.todoitem).value = '';
-	},
+    React.findDOMNode(this.refs.todoitem).value = '';
+  },
 
-	handleDelete: function(e){
-		var items = this.state.todoItems;
+  handleDelete: function(e){
+    var items = this.state.todoItems;
 
-		items.splice(e,1);
+    items.splice(e,1);
 
-		this.setState({
-			todoItems: items
-		});
-	},
+    this.setState({
+      todoItems: items
+    });
+  },
 
-	handleCompletion: function(e){
-		var items = this.state.todoItems;
+  handleCompletion: function(e){
+    var items = this.state.todoItems;
 
-		items[e].completed = true;
+    items[e].completed = true;
 
-		this.setState({
-			todoItems: items
-		});
+    this.setState({
+      todoItems: items
+    });
 
-	},
+  },
 
-	handleEdit: function(e){
+  handleEdit: function(e){
 
-		var items = this.state.todoItems;
+    var items = this.state.todoItems;
 
-		items[e].isEditMode = true;
+    items[e].isEditMode = true;
 
-		this.forceUpdate()
-	},
+    this.forceUpdate()
+  },
 
-	submitEdit: function(e){
-		e.preventDefault();
-	},
+  submitEdit: function(e){
+    e.preventDefault();
+  },
 
-	render: function() {
-		return (
-			<div className="container">
-			<h1 className="text-center">React Todo</h1>
-				<div className="row">
-					<div className="col-md-5 col-md-push-4">
-						<form onSubmit={this.handleSubmit}>
-						  <div className="form-group">
-						    <input ref="todoitem" type="text" className="form-control" id="todoitem" placeholder="Todo Item" />
-						  </div>
-						</form>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-md-5 col-md-push-4">
-						<TodoItems
-							todoItems={this.state.todoItems}
-							handleEdit={this.handleEdit}
-							submitEdit={this.submitEdit}
-							handleCompletion={this.handleCompletion}
-							handleDelete={this.handleDelete} />
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div className="container">
+      <h1 className="text-center">React Todo</h1>
+        <div className="row">
+          <div className="col-md-5 col-md-push-4">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <input ref="todoitem" type="text" className="form-control" id="todoitem" placeholder="Todo Item" />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-5 col-md-push-4">
+            <TodoItems
+              todoItems={this.state.todoItems}
+              handleEdit={this.handleEdit}
+              submitEdit={this.submitEdit}
+              handleCompletion={this.handleCompletion}
+              handleDelete={this.handleDelete} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 });
 
--- app/todoitems.js
+++ app/todoitems.js
@@ -2,43 +2,43 @@ var React = require('react');
 
 var TodoItems = React.createClass({
 
-	editMode: function(item){
-		return(
-			<form onSubmit={this.props.handleEdit}>
-				<input className="input" value={this.state.item} onChange={this.handleChange}></input>
-			</form>
-		);
-	},
+  editMode: function(item){
+    return(
+      <form onSubmit={this.props.handleEdit}>
+        <input className="input" value={this.state.item} onChange={this.handleChange}></input>
+      </form>
+    );
+  },
 
-	handleChange: function(e){
-		this.setState({
-			value: e.target.value
-		});
-	},
+  handleChange: function(e){
+    this.setState({
+      value: e.target.value
+    });
+  },
 
-	render: function() {
-		return (
-			<ul className="list-group">
-				{this.props.todoItems.map(function(item,i){
-					var defaultClass = item.completed ? "done" : "not-done";
+  render: function() {
+    return (
+      <ul className="list-group">
+        {this.props.todoItems.map(function(item,i){
+          var defaultClass = item.completed ? "done" : "not-done";
 
-					return(
-						<li key={i} className={defaultClass + ' list-group-item'} onDoubleClick={this.props.handleEdit.bind(null,i)}> 
-							{item.isEditMode ? this.editMode(item) : item}
-							<span className="pull-right"> 
-								<button onClick={this.props.handleCompletion.bind(null,i)} className="btn btn-success btn-xs">
-									<i className="fa fa-check"></i>
-								</button>
-								<button onClick={this.props.handleDelete.bind(null,i)} className="btn btn-danger btn-xs">
-									<i className="fa fa-times"></i>
-								</button>
-							</span>
-						</li>
-					);
-				},this)}
-			</ul>
-		);
-	}
+          return(
+            <li key={i} className={defaultClass + ' list-group-item'} onDoubleClick={this.props.handleEdit.bind(null,i)}> 
+              {item.isEditMode ? this.editMode(item) : item}
+              <span className="pull-right"> 
+                <button onClick={this.props.handleCompletion.bind(null,i)} className="btn btn-success btn-xs">
+                  <i className="fa fa-check"></i>
+                </button>
+                <button onClick={this.props.handleDelete.bind(null,i)} className="btn btn-danger btn-xs">
+                  <i className="fa fa-times"></i>
+                </button>
+              </span>
+            </li>
+          );
+        },this)}
+      </ul>
+    );
+  }
 
 });
 
